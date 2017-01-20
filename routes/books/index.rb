137a5939877d239ef49818f3e8261b101727f2a0
migrenaa <mimonova13@gmail.com>
2017-01-20 23:15:04 +0200

    view and books routes added

---
+++ routes/books/index.rb
@@ -0,0 +1,20 @@
+get '/' do
+  @books = Book.all
+
+  erb :index
+end
+
+post '/book' do
+  book = Book.new. title: params[:title], author: params[:author],
+      gender: params[:gender], description: params[:description]
+
+  if(book.save)
+    flash[:success] = 'Book saved.'
+  else
+    flash[:error] = "There is an error: #{book.errors.full_message.to_sentence}"
+  end
+  redirect '/'
+end
+
+
+
--- routes/books_controller.rb
+++
@@ -1,6 +0,0 @@
-require 'sinatra'
-class BooksController
-  get '/' do
-
-  end
-end
